1842c5cdc042c04763ede0f2237ab22d885eb593
180107072 <iokyufokyu@gmail.com>
2023-09-15 16:53:19 +0600

    fix deps

--- src/utils/get-url.ts
+++ src/utils/get-url.ts
@@ -1,6 +1,9 @@
 // utils/getURL.ts
 const IS_SERVER = typeof window === "undefined";
 export function getURL(path = "/") {
+  if (!process.env.NEXT_PUBLIC_SITE_URL) {
+    return `https://${process.env.VERCEL_URL!}`;
+  }
   const baseURL = IS_SERVER
     ? process.env.NEXT_PUBLIC_SITE_URL!
     : window.location.origin;
